289c3731818ec9ae15063ed99a7b7332b8f81f3b
Rayner Monteiro <monteirorayner@live.com>
2026-09-06 15:41:42 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp = {}  # Use a standard dict
+        res = 0
+
+        for num in nums:
+            if num not in mp:  # Now this works perfectly
+                # .get(key, 0) returns 0 if missing, without inserting it
+                left = mp.get(num - 1, 0)
+                right = mp.get(num + 1, 0)
+                
+                current_sum = left + right + 1
+                mp[num] = current_sum
+                
+                # Update the boundaries of the sequence
+                mp[num - left] = current_sum
+                mp[num + right] = current_sum
+                
+                res = max(res, current_sum)
+        return res
